f3214b2c4dc43cd0ff375393c361a8fcdfa11b42
notjrbauer <notjrbauer@gmail.com>
2016-04-09 17:21:30 -0400

    add track-request schema + tests

--- test.js
+++ test.js
@@ -434,3 +434,79 @@ test('Track Push Open Request', function (t) {
     t.end()
   })
 })
+
+test('Track Request', function (t) {
+  var trackReq = require('./track-request')
+  var validate = validator(trackReq)
+
+  t.test('is valid with email and minimum inputs', function (t) {
+    var isValid = validate({
+      email: 'test@test.com',
+      eventName: 'eventName'
+    })
+    t.ok(isValid)
+    t.end()
+  })
+
+  t.test('is valid with userId and minimum inputs', function (t) {
+    var isValid = validate({
+      userId: '1',
+      eventName: 'eventName'
+    })
+    t.ok(isValid)
+    t.end()
+  })
+
+  t.test('is invalid without email or userId', function (t) {
+    var isValid = validate({
+      eventName: 'eventName'
+    })
+    t.false(isValid)
+    t.end()
+  })
+
+  t.test('is valid with maximum optional inputs', function (t) {
+    var isValid = validate({
+      email: 'test@test.com',
+      userId: '1',
+      eventName: 'eventName',
+      campaignId: 1,
+      templateId: 1,
+      createdAt: Date.parse(Date.now()),
+      dataFields: {
+        date: 'fields'
+      }
+    })
+    t.ok(isValid)
+    t.end()
+  })
+
+  t.test('is invalid with missing inputs', function (t) {
+    var isValid = validate({
+      templateId: 1,
+      createdAt: Date.parse(Date.now()),
+      dataFields: {
+        date: 'fields'
+      }
+    })
+    t.false(isValid)
+    t.end()
+  })
+
+  t.test('is invalid with additional inputs', function (t) {
+    var isValid = validate({
+      email: 'test@test.com',
+      eventName: 'eventName',
+      userId: '1',
+      campaignId: 1,
+      templateId: 1,
+      createdAt: Date.parse(Date.now()),
+      dataFields: {
+        date: 'fields'
+      },
+      additional: 'input'
+    })
+    t.false(isValid)
+    t.end()
+  })
+})

563f05b6a1cc6c25355e0229b80b63954fd83048
notjrbauer <notjrbauer@gmail.com>
2016-04-09 17:12:55 -0400

    add trackPushOpenRequest and tests

--- test.js
+++ test.js
@@ -360,3 +360,77 @@ test('Track Conversion Request', function (t) {
     t.end()
   })
 })
+
+test('Track Push Open Request', function (t) {
+  var trackPushOpenReq = require('./track-push-open-request')
+  var validate = validator(trackPushOpenReq)
+
+  t.test('is valid with email and minimum inputs', function (t) {
+    var isValid = validate({
+      email: 'test@test.com',
+      campaignId: 1
+    })
+    t.ok(isValid)
+    t.end()
+  })
+
+  t.test('is valid with userId and minimum inputs', function (t) {
+    var isValid = validate({
+      userId: '1',
+      campaignId: 1
+    })
+    t.ok(isValid)
+    t.end()
+  })
+
+  t.test('is invalid without email or userId', function (t) {
+    var isValid = validate({
+      campaignId: 1
+    })
+    t.false(isValid)
+    t.end()
+  })
+
+  t.test('is valid with maximum optional inputs', function (t) {
+    var isValid = validate({
+      email: 'test@test.com',
+      userId: '1',
+      campaignId: 1,
+      templateId: 1,
+      createdAt: Date.parse(Date.now()),
+      dataFields: {
+        date: 'fields'
+      }
+    })
+    t.ok(isValid)
+    t.end()
+  })
+
+  t.test('is invalid with missing inputs', function (t) {
+    var isValid = validate({
+      templateId: 1,
+      createdAt: Date.parse(Date.now()),
+      dataFields: {
+        date: 'fields'
+      }
+    })
+    t.false(isValid)
+    t.end()
+  })
+
+  t.test('is invalid with additional inputs', function (t) {
+    var isValid = validate({
+      email: 'test@test.com',
+      userId: '1',
+      campaignId: 1,
+      templateId: 1,
+      createdAt: Date.parse(Date.now()),
+      dataFields: {
+        date: 'fields'
+      },
+      additional: 'input'
+    })
+    t.false(isValid)
+    t.end()
+  })
+})
---
+++ track-push-open-request.js
@@ -0,0 +1,35 @@
+module.exports = {
+  type: 'object',
+  properties: {
+    email: {
+      type: 'string',
+      format: 'email',
+      required: false
+    },
+    userId: {
+      type: 'string',
+      required: false
+    },
+    campaignId: {
+      type: 'number',
+      required: true
+    },
+    templateId: {
+      type: 'number',
+      required: false
+    },
+    createdAt: {
+      type: 'number',
+      required: false
+    },
+    dataFields: {
+      type: 'object',
+      required: false
+    }
+  },
+  anyOf: [
+    {'required': ['userId']},
+    {'required': ['email']}
+  ],
+  additionalProperties: false
+}

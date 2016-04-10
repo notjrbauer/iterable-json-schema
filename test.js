5a8f8edd7f5a77d41558f78483f50a267c0d2b7c
notjrbauer <notjrbauer@gmail.com>
2016-04-10 19:28:08 -0400

    add target sms request + tests

--- test.js
+++ test.js
@@ -803,3 +803,66 @@ test('Target Push Request', function (t) {
     t.end()
   })
 })
+
+test('Target SMS Request', function (t) {
+  var targetSmsReq = require('./target-sms-request')
+  var validate = validator(targetSmsReq)
+
+  t.test('is valid with email and minimum inputs', function (t) {
+    var isValid = validate({
+      campaignId: 1,
+      recipentEmail: 'test@test.com'
+    })
+    t.ok(isValid)
+    t.end()
+  })
+
+  t.test('is invalid without required inputs', function (t) {
+    var isValid = validate({
+      dataFields: {}
+    })
+    t.false(isValid)
+    t.end()
+  })
+
+  t.test('is valid with maximum optional inputs', function (t) {
+    var isValid = validate({
+      campaignId: 1,
+      recipentEmail: 'test@test.com',
+      dataFields: {
+        date: 'fields'
+      },
+      sendAt: new Date().toISOString()
+    })
+    t.ok(isValid)
+    t.end()
+  })
+
+  t.test('is invalid with additional inputs', function (t) {
+    var isValid = validate({
+      campaignId: 1,
+      recipentEmail: 'test@test.com',
+      dataFields: {
+        date: 'fields'
+      },
+      sendAt: new Date().toISOString(),
+      additional: 'data'
+    })
+    t.false(isValid)
+    t.end()
+  })
+
+  t.test('is invalid with non-isoformat string', function (t) {
+    var isValid = validate({
+      campaignId: 1,
+      recipentEmail: 'test@test.com',
+      dataFields: {
+        date: 'fields'
+      },
+      sendAt: new Date().toUTCString(),
+      additional: 'data'
+    })
+    t.false(isValid)
+    t.end()
+  })
+})

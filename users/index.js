7b1888324771bb20c5593056b1e5c5abac508596
notjrbauer <notjrbauer@gmail.com>
2016-04-10 23:50:00 -0400

    add users*

--- users/index.js
+++ users/index.js
@@ -1,19 +1,19 @@
 var validator = require('is-my-json-valid')
 
 var bulkUpdateReq = require('./bulk-update')
-var updateUserReq = require('./update-user')
 var updateEmailReq = require('./update-email')
 var disableDeviceReq = require('./disable-device')
 var updateSubReq = require('./update-subscription')
 
 var common = require('../common')
 var apiUser = common.apiUser
+var updateUser = common.updateUser
 
 var dependencies = {
   'api-user': apiUser
 }
 
-exports.updateUser = validator(updateUserReq)
+exports.updateUser = validator(updateUser)
 exports.updateEmail = validator(updateEmailReq)
 exports.disableDevice = validator(disableDeviceReq)
 exports.updateSubscription = validator(updateSubReq)

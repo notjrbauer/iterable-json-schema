77994ab37c625a7ff75aa97a532079d137c594ac
notjrbauer <notjrbauer@gmail.com>
2016-04-11 00:16:49 -0400

    export all the things

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 'use strict'
+
+module.exports = {
+  commerce: require('./commerce'),
+  workflow: require('./workflow'),
+  common: require('./common'),
+  events: require('./events'),
+  email: require('./email'),
+  lists: require('./lists'),
+  users: require('./users'),
+  push: require('./push'),
+  sms: require('./sms')
+}

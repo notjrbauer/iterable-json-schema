6075abf6c311d9ffcc4c728744daab6602ed10cc
notjrbauer <notjrbauer@gmail.com>
2016-04-10 20:09:42 -0400

    add track purchase request + tests

--- test.js
+++ test.js
@@ -864,3 +864,104 @@ test('Target SMS Request', function (t) {
     t.end()
   })
 })
+
+test('Track Purchase Request', function (t) {
+  var trackPurchaseReq = require('./track-purchase-request')
+  var user = require('./api-user-update-request')
+  var item = require('./commerce-item')
+  var validate = validator(trackPurchaseReq, {
+    schemas: {
+      'api-user-update-request': user,
+      'commerce-item': item
+    }
+  })
+
+  t.test('is valid with minimum inputs', function (t) {
+    var isValid = validate({
+      user: {
+        email: 'test@test.com'
+      },
+      items: [{
+        id: '1',
+        name: 'item one',
+        price: 100,
+        quantity: 1
+      }],
+      total: 100
+    })
+    t.ok(isValid)
+    t.end()
+  })
+
+  t.test('is valid with maximum inputs', function (t) {
+    var isValid = validate({
+      user: {
+        email: 'test@test.com'
+      },
+      campaignId: 1,
+      templateId: 2,
+      createdAt: Date.parse(new Date()),
+      dataFields: {},
+      items: [{
+        id: '1',
+        name: 'item one',
+        price: 100,
+        quantity: 1
+      }],
+      total: 100
+    })
+    t.ok(isValid)
+    t.end()
+  })
+
+  t.test('is invalid with missing required inputs', function (t) {
+    var isValid = validate({
+      campaignId: 1,
+      templateId: 2,
+      createdAt: Date.parse(new Date()),
+      dataFields: {},
+      total: 100
+    })
+    t.false(isValid)
+    t.end()
+  })
+
+  t.test('is invalid with additional inputs', function (t) {
+    var isValid = validate({
+      user: {
+        email: 'test@test.com'
+      },
+      campaignId: 1,
+      templateId: 2,
+      createdAt: Date.parse(new Date()),
+      dataFields: {},
+      items: [{
+        id: '1',
+        name: 'item one',
+        price: 100,
+        quantity: 1
+      }],
+      total: 100,
+      additional: 'inputs'
+    })
+    t.false(isValid)
+    t.end()
+  })
+
+  t.test('is invalid with 0 items', function (t) {
+    var isValid = validate({
+      user: {
+        email: 'test@test.com'
+      },
+      campaignId: 1,
+      templateId: 2,
+      createdAt: Date.parse(new Date()),
+      dataFields: {},
+      items: [],
+      total: 100,
+      additional: 'inputs'
+    })
+    t.false(isValid)
+    t.end()
+  })
+})

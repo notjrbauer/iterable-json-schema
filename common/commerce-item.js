11a0830469026b18a9ed2f237d842a9aeca59ee2
notjrbauer <notjrbauer@gmail.com>
2016-06-03 14:03:09 -0700

    Allow nullable

--- common/commerce-item.js
+++ common/commerce-item.js
@@ -2,43 +2,43 @@ module.exports = {
   type: 'object',
   properties: {
     id: {
-      type: 'string',
+      type: ['string', 'null'],
       required: true
     },
     sku: {
-      type: 'string',
+      type: ['string', 'null'],
       required: false
     },
     name: {
-      type: 'string',
+      type: ['string', 'null'],
       required: true
     },
     description: {
-      type: 'string',
+      type: ['string', 'null'],
       required: false
     },
     categories: {
-      type: 'array',
+      type: ['array', 'null'],
       required: false
     },
     price: {
-      type: 'number',
+      type: ['number', 'null'],
       required: true
     },
     quantity: {
-      type: 'number',
+      type: ['number', 'null'],
       required: true
     },
     imageUrl: {
-      type: 'string',
+      type: ['string', 'null'],
       required: false
     },
     url: {
-      type: 'string',
+      type: ['string', 'null'],
       required: false
     },
     dataFields: {
-      type: 'object',
+      type: ['object', 'null'],
       required: false
     }
   },
